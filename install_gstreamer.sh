4cf682c7687e690a66eb0aaabb0702aaf4931f7a
Jorma <jorma.rebane@gmail.com>
2020-12-07 00:48:35 +0200

    More gstreamer samples

---
+++ install_gstreamer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install gstreamer1.0-tools gstreamer1.0-plugins-rtp \
+ gstreamer1.0-plugins-good gstreamer1.0-gl gstreamer1.0-x \
+ gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+
+sudo apt-get install gstreamer1.0-omx-generic libomxil-bellagio
--- play_device0.py
+++ play_device0.py
@@ -1,7 +1,74 @@
 #!/usr/bin/python3
 import os
 
+#v4l2-ctl -d /dev/v4l/by-id/usb-MACROSILICON_AV_TO_USB2.0_20150130-video-index0 --list-formats
+#/dev/v4l/by-id/usb-MACROSILICON_AV_TO_USB2.0_20150130-video-index0
+#/dev/v4l/by-id/usb-fushicai_usbtv007_300000000002-video-index0
 video_device = "/dev/video0"
+
+thermal_cam = "/dev/v4l/by-id/usb-MACROSILICON_AV_TO_USB2.0_20150130-video-index0"
+rgb_cam = "/dev/v4l/by-id/usb-fushicai_usbtv007_300000000002-video-index0"
+
+clock = "clockoverlay halignment=right valignment=bottom time-format=\"%Y/%m/%d %H:%M:%S\""
+
+
+# v4l2h264enc
+gst = "gst-launch-1.0 -v"
+gst_thr = f"v4l2src name=src device={thermal_cam} do-timestamp=true"\
+       " ! capsfilter caps=image/jpeg,width=640,height=480,framerate=25/1"\
+       " ! jpegdec"
+
+gst_rgb = f"v4l2src name=src device={rgb_cam} do-timestamp=true"\
+      " ! \"video/x-raw,format=YUY2,width=800,height=600,framerate=20/1\""
+
+gst_test = f"videotestsrc ! video/x-raw,width=640,height=480"
+
+imagesink = f"videoconvert ! {clock} ! ximagesink sync=false"
+
+
+############
+# intra-refresh=true # lower latency???
+enc_opt = "b-adapt=false bitrate=2200 ref=1"\
+          " speed-preset=superfast vbv-buf-capacity=100"
+encode_x264  = f"x264enc {enc_opt} ! h264parse config-interval=-1"
+
+enc_opt = "target-bitrate=2200 quant-b-frames=0"
+encode_omx = f"omxh264enc {enc_opt}"
+
+enc_opt = "max-bframes=0 bitrate=2200000"
+encode_av264 = f"avenc_h264_omx {enc_opt}"
+
+encode = encode_x264
+###########
+
+# aggregate-mode=zero-latency
+rtp_pay = "rtph264pay pt=96 mtu=1000 config-interval=1"
+udpsink = "udpsink host=192.168.1.4 port=6666"
+udpsrc = "udpsrc port=6666"
+
+rtp_depay_decode = "rtph264depay ! avdec_h264"
+
+os.system(f"{gst} {gst_thr} ! {encode} ! {rtp_pay} ! {udpsink}")
+
+#os.system(f"{udpsrc} ! {rtp_depay_decode} ! {imagesink}")
+#os.system(f"{gst_rgb} ! {imagesink}")
+#os.system(f"{}")
+
+#os.system(f"{raw} | mplayer -")
+#os.system(f"{encode} | mplayer -")
+#os.system(f"{encode} | ffplay -nostats -fflags nobuffer -")
+
+#gst-launch -v udpsrc port=5000 caps="<CAPS_FROM_SERVER>" ! rtph264depay ! ffdec_h264 ! xvimagesink
+
+easycap = "/root/easycap-somagic-linux/somagic-capture"\
+          " --sync=1 --iso-transfers 20 -B 149 -C 72 --luminance 2 --lum-aperture 3"
+gste = f"{easycap} |"\
+      "gst-launch-1.0 fdsrc fd=0"\
+      " ! capsfilter caps=image/jpeg,width=640,height=480,framerate=25/1"\
+      " ! jpegdec ! videoconvert ! videoscale "\
+      " ! clockoverlay halignment=right valignment=bottom time-format=\"%Y/%m/%d %H:%M:%S\""\
+      " ! ximagesink sync=false"
+
 
 ffmpeg = "/usr/bin/ffmpeg -nostats -loglevel warning"
 
@@ -13,18 +80,3 @@ encode = f"{ffmpeg} -y -an -r ntsc -i {video_device}" \
 
 raw = f"{ffmpeg} -y -an -r ntsc -i {video_device}" \
     f" -c:v libx264 -preset ultrafast -pix_fmt yuv420p -filter fps=fps=25 -f matroska -"
-
-gst = f"gst-launch-1.0 v4l2src name=src device=/dev/video0"\
-      " ! capsfilter caps=image/jpeg,width=640,height=480,framerate=25/1"\
-      " ! jpegdec ! videoconvert"\
-      " ! timeoverlay halignment=right valignment=bottom"\
-      " ! ximagesink"
-
-gst2 = f"gst-launch-1.0 videotestsrc ! ximagesink"
-
-os.system(f"{gst}")
-#os.system(f"{gst2}")
-
-#os.system(f"{raw} | mplayer -")
-#os.system(f"{encode} | mplayer -")
-#os.system(f"{encode} | ffplay -nostats -fflags nobuffer -")
