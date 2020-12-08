ee6a08bc22c2e98922da874fdf2b136676954105
Jorma <jorma.rebane@gmail.com>
2020-12-08 04:31:49 +0200

    updlisten somewhat working, a few invalid packets

--- udplisten.py
+++ udplisten.py
@@ -2,13 +2,8 @@
 import os, pathlib
 
 port = 5600
-
-caps = pathlib.Path('caps.rtp').read_text()
-
-caps = 'application/x-rtp,media=video,payload=96,encoding-name=H264,'\
-       'packetization-mode=1,profile-level-id=64001e,'\
-       'sprop-parameter-sets="Z2QAHqy0BQHtgLUGAQWlAAADAAEAAAMAPI8WLqA\\=\\,aO88sA\\=\\=",'\
-       'ssrc=3448613135,timestamp-offset=753121922,seqnum-offset=19603,a-framerate=30'
+caps = 'application/x-rtp,media=video,payload=96,encoding-name=H264'
+#caps = pathlib.Path('caps.rtp').read_text()
 
 udp = f'udpsrc port={port} ! {caps}'
 clock = "clockoverlay halignment=right valignment=bottom time-format=\"%Y/%m/%d %H:%M:%S\""
